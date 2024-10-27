ee5ad7cf8be05e8a70670ea04289abba31560e21
camelCase <Bhagatakansha154@gmail.com>
2024-10-27 16:43:35 +0530

    excel controller deleted
    obsolete files deleted

--- routes/excelRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { uploadExcelData } from '../controllers/excelUploadController.js';
-
-const router = express.Router();
-
-router.post('/upload', uploadExcelData);
-
-export default router;
